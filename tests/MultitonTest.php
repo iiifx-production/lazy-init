b207ec8ef0b80810d5b0953ab24d6ef07018eb4c
IIIFX <iiifx@yandex.ua>
2015-07-12 20:04:09 +0300

    Добавил тесты

---
+++ tests/MultitonTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use iiifx\LazyInit\LazyInitStaticTrait;
+
+class MultitonTest extends PHPUnit_Framework_TestCase {
+
+    use LazyInitStaticTrait;
+
+    function testMultiton () {
+        $this->assertEquals( LazyInitStaticTraitTest_Multiton::getInstance( 'key-1' )->key, 'key-1' );
+        $this->assertEquals( LazyInitStaticTraitTest_Multiton::getInstance( 'key-1' )->key, 'key-1' );
+        $this->assertEquals( LazyInitStaticTraitTest_Multiton::getInstance( 'key-2' )->key, 'key-2' );
+        $this->assertEquals( LazyInitStaticTraitTest_Multiton::getInstance( 'key-1' )->key, 'key-1' );
+    }
+
+}
+
+/**
+ * Class LazyInitStaticTraitTest_Multiton
+ *
+ * Для теста
+ */
+class LazyInitStaticTraitTest_Multiton {
+
+    use LazyInitStaticTrait;
+
+    private function __clone () {}
+    private function __wakeup () {}
+
+    public $key;
+
+    private function __construct ( $key ) {
+        $this->key = $key;
+    }
+
+    /**
+     * @param $key
+     *
+     * @return static
+     */
+    public static function getInstance ( $key ) {
+        return static::lazyInitStatic( function ( $key ) {
+            return new static( $key );
+        }, $key, [ $key ] );
+    }
+
+}
---
+++ tests/SingletonTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use iiifx\LazyInit\LazyInitStaticTrait;
+
+class SingletonTest extends PHPUnit_Framework_TestCase {
+
+    use LazyInitStaticTrait;
+
+    function testSingleton () {
+        $this->assertEquals( LazyInitStaticTraitTest_Singleton::getInstance( 'key' )->key, 'key' );
+        $this->assertEquals( LazyInitStaticTraitTest_Singleton::getInstance( 'key' )->key, 'key' );
+        $this->assertEquals( LazyInitStaticTraitTest_Singleton::getInstance( 'wrong-key' )->key, 'key' );
+        $this->assertEquals( LazyInitStaticTraitTest_Singleton::getInstance( 'key' )->key, 'key' );
+    }
+
+}
+
+/**
+ * Class LazyInitStaticTraitTest_Singleton
+ *
+ * Для теста
+ */
+class LazyInitStaticTraitTest_Singleton {
+
+    use LazyInitStaticTrait;
+
+    private function __clone () {}
+    private function __wakeup () {}
+
+    public $key;
+
+    private function __construct ( $key ) {
+        $this->key = $key;
+    }
+
+    /**
+     * @param $key
+     *
+     * @return static
+     */
+    public static function getInstance ( $key ) {
+        return static::lazyInitStatic( function ( $key ) {
+            return new static( $key );
+        }, __METHOD__, [ $key ] );
+    }
+
+}
